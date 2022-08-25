a39efaeff2ab21810d889e9241f9ab76dd8ff369
jeewanthamadu <jeewanthamadu2000@gmail.com>
2022-08-25 23:41:10 +0530

    order Detail part

--- routes/item.js
+++ routes/item.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db =require(".../config/db.config");
+const db =require("../config/db.config");
 
 const mysql =require("mysql");
 const req = require("express/lib/request");
